9ce4f8677faab359e7fa905622e4151bee66a91d
laura-e24 <72033353+laura-e24@users.noreply.github.com>
2023-04-10 17:02:13 -0300

    Java día 3: condicionales

--- src/tutorial/Class2.java
+++ src/tutorial/Class2.java
@@ -1,7 +1,26 @@
 package src.tutorial;
 
+import java.util.Scanner;
+
 public class Class2 {
+  final static Scanner input = new Scanner(System.in);
+  final static String REQUEST_MESSAGE = "Please, enter the number ";
+  final static String RESPONSE_MESSAGE = "Highest number is... ";
+  final static String RESPONSE_MESSAGE_EQUALS = "Numbers are equal.";
   public static void main(String[] args) {
-    
+    int num1;
+    int num2;
+
+    System.out.println(REQUEST_MESSAGE + "one:");
+    num1 = input.nextInt();
+
+    System.out.println(REQUEST_MESSAGE + "two:");
+    num2 = input.nextInt();
+
+    if (num1 > num2) System.out.println(RESPONSE_MESSAGE + num1);
+    else if (num2 > num1) System.out.println(RESPONSE_MESSAGE + num2);
+    else System.out.println(RESPONSE_MESSAGE_EQUALS);
+
+    input.close();
   }
 }
